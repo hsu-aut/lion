cb644de803557951c70251a22ce35746036952ef
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2019-05-21 15:38:23 +0200

    omitted redundancy in methods

--- devApp/src/app/components/dinen61360/dinen61360.component.ts
+++ devApp/src/app/components/dinen61360/dinen61360.component.ts
@@ -109,97 +109,18 @@ export class Dinen61360Component implements OnInit {
   }
 
   buildTypeInsert() {
-
-    var varia: DINEN61360Variables = {
-      optionalTypeVariables:
-      {
-        Synonymous_Name: this.Synonymous_Name,
-        backwards_compatible_Revision: this.backwards_compatible_Revision,
-        backwards_compatible_Version: this.backwards_compatible_Version,
-        Value_Format_Field_length: this.Value_Format_Field_length,
-        Value_Format_Field_length_Variable: this.Value_Format_Field_length_Variable,
-        Value_Format_non_quantitativ: this.Value_Format_non_quantitativ,
-        Value_Format_quantitativ: this.Value_Format_quantitativ,
-        Value_List: this.Value_List,
-        Value_List_Member: this.Value_List_Member,
-        Source_Document_of_Definition: this.Source_Document_of_Definition,
-        Synonymous_Letter_Symbol: this.Synonymous_Letter_Symbol,
-        Note: this.Note,
-        Remark: this.Remark,
-        Preferred_Letter_Symbol: this.Preferred_Letter_Symbol,
-        Formula: this.Formula,
-        Drawing_Reference: this.Drawing_Reference,
-      },
-      mandatoryTypeVariables: {
-        code: this.code,
-        version: this.version,
-        revision: this.revision,
-        preferredName: this.preferred_name,
-        shortName: this.short_name,
-        definition: this.definition,
-        datatypeString: this.selectedDataytpe,
-        unitOfMeasure: this.UoM
-      },
-      instanceVariables: {
-        expressionGoalString: this.selectedExpressionGoal,
-        logicInterpretationString: this.selectedLogicInterpretation,
-        valueString: this.value,
-        describedIndividual: this.describedIndividual
-      }
-    }
+    var varia = this.getVariables();
     this.insertString = this.din.buildDINEN61360T(varia);
-
-
     const blob = new Blob([this.insertString], { type: 'text/plain' });
-    // Dateiname
     const name = 'insert.txt';
     this.dlService.download(blob, name);
 
   }
 
   buildInstanceInsert() {
-    var varia: DINEN61360Variables = {
-      optionalTypeVariables:
-      {
-        Synonymous_Name: this.Synonymous_Name,
-        backwards_compatible_Revision: this.backwards_compatible_Revision,
-        backwards_compatible_Version: this.backwards_compatible_Version,
-        Value_Format_Field_length: this.Value_Format_Field_length,
-        Value_Format_Field_length_Variable: this.Value_Format_Field_length_Variable,
-        Value_Format_non_quantitativ: this.Value_Format_non_quantitativ,
-        Value_Format_quantitativ: this.Value_Format_quantitativ,
-        Value_List: this.Value_List,
-        Value_List_Member: this.Value_List_Member,
-        Source_Document_of_Definition: this.Source_Document_of_Definition,
-        Synonymous_Letter_Symbol: this.Synonymous_Letter_Symbol,
-        Note: this.Note,
-        Remark: this.Remark,
-        Preferred_Letter_Symbol: this.Preferred_Letter_Symbol,
-        Formula: this.Formula,
-        Drawing_Reference: this.Drawing_Reference,
-      },
-      mandatoryTypeVariables: {
-        code: this.code,
-        version: this.version,
-        revision: this.revision,
-        preferredName: this.preferred_name,
-        shortName: this.short_name,
-        definition: this.definition,
-        datatypeString: this.selectedDataytpe,
-        unitOfMeasure: this.UoM
-      },
-      instanceVariables: {
-        expressionGoalString: this.selectedExpressionGoal,
-        logicInterpretationString: this.selectedLogicInterpretation,
-        valueString: this.value,
-        describedIndividual: this.describedIndividual
-      }
-    }
+    var varia = this.getVariables();
     this.insertString = this.din.buildDINEN61360I(varia);
-    // new: Hamied -> Download insertString as .txt file
-    // Blob
     const blob = new Blob([this.insertString], { type: 'text/plain' });
-    // Dateiname
     const name = 'insert.txt';
     this.dlService.download(blob, name);
   }
@@ -246,43 +167,7 @@ export class Dinen61360Component implements OnInit {
     this.code = row.ID;
   }
   insertDINEN61360T() {
-    var varia: DINEN61360Variables = {
-      optionalTypeVariables:
-      {
-        Synonymous_Name: this.Synonymous_Name,
-        backwards_compatible_Revision: this.backwards_compatible_Revision,
-        backwards_compatible_Version: this.backwards_compatible_Version,
-        Value_Format_Field_length: this.Value_Format_Field_length,
-        Value_Format_Field_length_Variable: this.Value_Format_Field_length_Variable,
-        Value_Format_non_quantitativ: this.Value_Format_non_quantitativ,
-        Value_Format_quantitativ: this.Value_Format_quantitativ,
-        Value_List: this.Value_List,
-        Value_List_Member: this.Value_List_Member,
-        Source_Document_of_Definition: this.Source_Document_of_Definition,
-        Synonymous_Letter_Symbol: this.Synonymous_Letter_Symbol,
-        Note: this.Note,
-        Remark: this.Remark,
-        Preferred_Letter_Symbol: this.Preferred_Letter_Symbol,
-        Formula: this.Formula,
-        Drawing_Reference: this.Drawing_Reference,
-      },
-      mandatoryTypeVariables: {
-        code: this.code,
-        version: this.version,
-        revision: this.revision,
-        preferredName: this.preferred_name,
-        shortName: this.short_name,
-        definition: this.definition,
-        datatypeString: this.selectedDataytpe,
-        unitOfMeasure: this.UoM
-      },
-      instanceVariables: {
-        expressionGoalString: this.selectedExpressionGoal,
-        logicInterpretationString: this.selectedLogicInterpretation,
-        valueString: this.value,
-        describedIndividual: this.describedIndividual
-      }
-    }
+    var varia = this.getVariables();
     this.insertString = this.din.buildDINEN61360T(varia);
     this.query.insert(this.insertString).subscribe((data: any) => {
       console.log(data)
@@ -292,43 +177,7 @@ export class Dinen61360Component implements OnInit {
 
   }
   insertDINEN61360I() {
-    var varia: DINEN61360Variables = {
-      optionalTypeVariables:
-      {
-        Synonymous_Name: this.Synonymous_Name,
-        backwards_compatible_Revision: this.backwards_compatible_Revision,
-        backwards_compatible_Version: this.backwards_compatible_Version,
-        Value_Format_Field_length: this.Value_Format_Field_length,
-        Value_Format_Field_length_Variable: this.Value_Format_Field_length_Variable,
-        Value_Format_non_quantitativ: this.Value_Format_non_quantitativ,
-        Value_Format_quantitativ: this.Value_Format_quantitativ,
-        Value_List: this.Value_List,
-        Value_List_Member: this.Value_List_Member,
-        Source_Document_of_Definition: this.Source_Document_of_Definition,
-        Synonymous_Letter_Symbol: this.Synonymous_Letter_Symbol,
-        Note: this.Note,
-        Remark: this.Remark,
-        Preferred_Letter_Symbol: this.Preferred_Letter_Symbol,
-        Formula: this.Formula,
-        Drawing_Reference: this.Drawing_Reference,
-      },
-      mandatoryTypeVariables: {
-        code: this.code,
-        version: this.version,
-        revision: this.revision,
-        preferredName: this.preferred_name,
-        shortName: this.short_name,
-        definition: this.definition,
-        datatypeString: this.selectedDataytpe,
-        unitOfMeasure: this.UoM
-      },
-      instanceVariables: {
-        expressionGoalString: this.selectedExpressionGoal,
-        logicInterpretationString: this.selectedLogicInterpretation,
-        valueString: this.value,
-        describedIndividual: this.describedIndividual
-      }
-    }
+    var varia = this.getVariables();
     this.insertString = this.din.buildDINEN61360I(varia);
     this.query.insert(this.insertString).subscribe((data: any) => {
       this.getStatisticInfo();
@@ -375,5 +224,46 @@ export class Dinen61360Component implements OnInit {
       this.namespaceParser.parseToPrefix(data);
       this.NoOfDEI = this.TableUtil.buildTable(data).length;
     });
+  }
+
+  getVariables(){
+    var varia: DINEN61360Variables = {
+      optionalTypeVariables:
+      {
+        Synonymous_Name: this.Synonymous_Name,
+        backwards_compatible_Revision: this.backwards_compatible_Revision,
+        backwards_compatible_Version: this.backwards_compatible_Version,
+        Value_Format_Field_length: this.Value_Format_Field_length,
+        Value_Format_Field_length_Variable: this.Value_Format_Field_length_Variable,
+        Value_Format_non_quantitativ: this.Value_Format_non_quantitativ,
+        Value_Format_quantitativ: this.Value_Format_quantitativ,
+        Value_List: this.Value_List,
+        Value_List_Member: this.Value_List_Member,
+        Source_Document_of_Definition: this.Source_Document_of_Definition,
+        Synonymous_Letter_Symbol: this.Synonymous_Letter_Symbol,
+        Note: this.Note,
+        Remark: this.Remark,
+        Preferred_Letter_Symbol: this.Preferred_Letter_Symbol,
+        Formula: this.Formula,
+        Drawing_Reference: this.Drawing_Reference,
+      },
+      mandatoryTypeVariables: {
+        code: this.code,
+        version: this.version,
+        revision: this.revision,
+        preferredName: this.preferred_name,
+        shortName: this.short_name,
+        definition: this.definition,
+        datatypeString: this.selectedDataytpe,
+        unitOfMeasure: this.UoM
+      },
+      instanceVariables: {
+        expressionGoalString: this.selectedExpressionGoal,
+        logicInterpretationString: this.selectedLogicInterpretation,
+        valueString: this.value,
+        describedIndividual: this.describedIndividual
+      }
+    }
+    return varia
   }
 }
