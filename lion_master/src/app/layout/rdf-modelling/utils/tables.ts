f871068a28c7553c7801fae741e08d5149d402e0
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2019-06-14 17:02:26 +0200

    added list to table functionality
    one can concat lists to tables now with concatListsToTable(colNames: Array<string>, listData: Array<Array<string>>)

--- lion_master/src/app/layout/rdf-modelling/utils/tables.ts
+++ lion_master/src/app/layout/rdf-modelling/utils/tables.ts
@@ -39,5 +39,37 @@ export class Tables {
 
     }
 
+    concatListsToTable(colNames: Array<string>, listData: Array<Array<string>>){
+        var table: Array<Object> = []
+        let cols = colNames;
+        let data = listData;
+        let length = 0;
+
+        // get max length
+        for (let i = 0; i < data.length; i++) {
+          if (data[i].length > length) { length = data[i].length }
+        }
+    
+        for (let i = 0; i < length; i++) {
+          let row = {}
+          for (let head = 0; head < cols.length; head++) {
+            let colname = cols[head]
+            let cellEntry = ""
+            row[colname] = cellEntry
+          } table.push(row)
+        }
+
+    
+        for (let i = 0; i < data.length; i++) {
+          let colData = data[i];
+          for (let ii = 0; ii < colData.length; ii++) {
+            let key = Object.keys(table[ii])[i]
+            table[ii][key] = colData[ii];
+          }
+        }
+
+        return table
+    }
+
 
 }
