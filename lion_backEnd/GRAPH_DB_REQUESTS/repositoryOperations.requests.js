d2bb7ab604f4542f4a43bc22e06544f6cb52c93a
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2019-11-25 15:15:48 +0100

    Update repositoryOperations.requests.js
    added get list of repositories function

--- lion_backEnd/GRAPH_DB_REQUESTS/repositoryOperations.requests.js
+++ lion_backEnd/GRAPH_DB_REQUESTS/repositoryOperations.requests.js
@@ -1,6 +1,47 @@
 const client = require('axios');
 
-exports.clearRepository = function (repositoryName) {
+
+/* method to get a list of repositories via HTTP GET from the GRaphDB */
+exports.GET_REPOSITORIES = function () {
+
+
+  var promise = new Promise(function (resolve) {
+
+    var config = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/sparql-results+json, */*;q=0.5'
+      },
+      baseURL: 'http://localhost:7200/',
+      url: `/repositories`
+    }
+
+    client(config).then(function (response) {
+      console.log("Got a response from GraphDB");
+      let GDB = {
+          status: response.status,
+          data: JSON.stringify(response.data)
+      }
+      resolve(GDB);
+    })
+      .catch(function (error) {
+        console.log("Got an error from GraphDB");
+        let GDB = {
+            status: error.response.status,
+            data: error.response.data
+        }
+        console.log(error);
+        console.log(GDB)
+        resolve(GDB);
+      });
+
+  });
+  return promise
+
+}
+
+/* method to clear a repository via HTTP DELETE in the GRaphDB */
+exports.CLEAR_REPOSITORY = function (repositoryName) {
 
 
     var promise = new Promise(function (resolve) {
@@ -12,12 +53,22 @@ exports.clearRepository = function (repositoryName) {
       }
   
       client(config).then(function (response) {
-        console.log("Got a response from GraphDB " + response.status);
-        resolve(response);
+        console.log("Got a response from GraphDB");
+        let GDB = {
+            status: response.status,
+            data: JSON.stringify(response.data)
+        }
+        resolve(GDB);
       })
         .catch(function (error) {
-          reject(error);
+          console.log("Got an error from GraphDB");
+          let GDB = {
+              status: error.response.status,
+              data: error.response.data
+          }
           console.log(error);
+          console.log(GDB)
+          resolve(GDB);
         });
   
     });
