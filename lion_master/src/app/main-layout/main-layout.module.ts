fe0f3805dc382878546fecea4b1d0677beb4e938
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2019-11-27 10:03:03 +0100

    added loader and messages to main layout

--- lion_master/src/app/main-layout/main-layout.module.ts
+++ lion_master/src/app/main-layout/main-layout.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
 import { MainLayoutComponent } from './main-layout.component';
 
 import { MainLayoutRoutingModule } from './main-layout-routing.module';
 
 // child components
 import { HeaderComponent } from './head-nav/header.component';
+import { LoaderComponent } from '../shared/loader/loader.component';
+import { MessagesComponent } from '../shared/messages/messages.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainLayoutRoutingModule
+    MainLayoutRoutingModule,
+    NgbCarouselModule,
+    NgbAlertModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     MainLayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoaderComponent,
+    MessagesComponent
   ]
 })
 export class MainLayoutModule { }
