df9767d166ae9ebb9befdf82441f1a38846380f8
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2019-11-27 14:31:09 +0100

    created abox explorer

---
+++ lion_master/src/app/exploration/abox-explorer/abox-explorer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abox-explorer',
+  templateUrl: './abox-explorer.component.html',
+  styleUrls: ['./abox-explorer.component.scss']
+})
+export class AboxExplorerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
