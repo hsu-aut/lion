638d30cd8694f4240ef126d3a3af1f361bda5123
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2020-02-18 09:34:48 +0100

    minor updates

--- lion_master/src/app/mapping/step/step-service.service.ts
+++ lion_master/src/app/mapping/step/step-service.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
-import { ConfigurationService } from '../../shared/services/backEnd/configuration.service'
+import { ConfigurationService } from '../../shared/services/backEnd/configuration.service';
+import { GraphOperationsService } from '../../shared/services/backEnd/graphOperations.service';
+import { RepositoryOperationsService } from '../../shared/services/backEnd/repositoryOperations.service';
+import { MessagesService } from '../../shared/services/messages.service';
+
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +18,10 @@ export class StepServiceService {
 
   constructor(
     private config: ConfigurationService,
-    private http: HttpClient
+    private http: HttpClient,
+    private graphs: GraphOperationsService,
+    private repos: RepositoryOperationsService,
+    private messageService: MessagesService
   ) { }
 
 
@@ -33,6 +41,13 @@ export class StepServiceService {
     return this.http.get(this.config.getHost() + this.stepRoute, httpOptions);
   }
 
+  /**
+   * Method takes a filename and requests to delete that file in the backend.
+   *
+   * @param {string} fileName
+   * @returns Observable of HTTP request
+   * @memberof StepServiceService
+   */
   deleteFile(fileName: string) {
     var httpOptions = {
       headers: new HttpHeaders({
@@ -50,6 +65,51 @@ export class StepServiceService {
     return this.http.delete(request, httpOptions);
   }
 
+
+
+  /**
+   * Method requests to map a stp file to rdf by the backend.
+   *
+   * @param {string} fileName
+   * @returns Observable of HTTP request
+   * @memberof StepServiceService
+   */
+  mapToRDF(fileName: string) {
+
+    let request: string = this.config.getHost() + this.stepRoute + '/rdf';
+
+    let body = {
+      fileName: fileName,
+      activeGraph: this.graphs.getGraphs()[this.graphs.getActiveGraph()],
+      repositoryName: this.repos.getRepository()
+    }
+    
+
+    let headers = new HttpHeaders().set('Content-Type', 'application/json').set('Accept', 'application/json')
+    
+
+
+    var insertObservable = new Observable((observer) => {
+      this.http.put(request, body, { headers }).pipe(take(1)).subscribe((data: any) => {
+        this.messageService.addMessage('success', 'Alright!', `Backend is processing the file. This may take a while.`);
+        observer.next()
+        observer.complete()
+      },
+        error => {
+          this.messageService.addMessage('error', 'Ups!', `Seams like the Server responded with a ${error.status} code`);
+        });
+    })
+
+    return insertObservable;
+  }
+
+  /**
+   * Method takes a filename and requests a a mapping from .step to json by the back end.
+   *
+   * @param {string} fileName
+   * @returns Observable of the HTTP request, a json should be returned by the backend
+   * @memberof StepServiceService
+   */
   getJson(fileName: string) {
     var httpOptions = {
       headers: new HttpHeaders({
@@ -58,7 +118,7 @@ export class StepServiceService {
     };
     var request: string;
 
-      request = this.config.getHost() + this.stepRoute + '/tojson' +`?fileName=${fileName}`;
+    request = this.config.getHost() + this.stepRoute + '/json' + `?fileName=${fileName}`;
 
     return this.http.get(request, httpOptions);
   }
--- lion_master/src/app/mapping/step/step.component.ts
+++ lion_master/src/app/mapping/step/step.component.ts
@@ -30,7 +30,9 @@ export class StepComponent implements OnInit {
   }
 
   mapToRDF(file: string) {
-
+    this.step.mapToRDF(file).pipe(take(1)).subscribe((data: any) => {
+      console.log(data);
+    });
   }
 
   mapModifiedToRDF(file: string) {
