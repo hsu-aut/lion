2dfb20857bdcb4172d5d16b015492c1bf68f5825
Constantin1337 email = c.hildebrandt@hsu-hh.de [core] <c.hildebrandt@hsu-hh.de>
2020-02-20 14:48:42 +0100

    lovely fpb mapping component

--- lion_master/src/app/mapping/fpb/fpb.component.ts
+++ lion_master/src/app/mapping/fpb/fpb.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FpbService } from './fpb.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-fpb',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FpbComponent implements OnInit {
 
-  constructor() { }
+  private fpbURL = this.fpb.fpbRoute;
+  private fpbJSON = {}
+  private uploadedFiles = ["Do you have a connection to the backend?"]
+  private fileType = [".json"]
+
+  constructor(
+    private fpb: FpbService
+  ) { }
 
   ngOnInit() {
+    this.getListofFiles();
   }
+
+  getListofFiles() {
+    this.fpb.getListOfFiles().pipe(take(1)).subscribe((data: any) => {
+      this.uploadedFiles = data;
+    });
+  }
+
+  mapToRDF(file: string) {
+    this.fpb.mapToRDF(file).pipe(take(1)).subscribe((data: any) => {
+      console.log(data);
+    });
+  }
+
+
+
+  deleteFile(file: string) {
+    this.fpb.deleteFile(file).pipe(take(1)).subscribe((data: any) => {
+      console.log(data);
+      this.getListofFiles()
+    });
+  }
+
 
 }
