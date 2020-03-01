053eb95dc2d7da51cd7c500545b5d7770d245936
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-03-01 17:10:00 +0100

    Imported ReactiveFormsModule

--- lion_master/src/app/mapping/mapping.module.ts
+++ lion_master/src/app/mapping/mapping.module.ts
@@ -8,7 +8,7 @@ import { MappingRoutingModule } from './mapping-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { OpcComponent } from './opc/opc.component';
 import { R2rmlComponent } from './r2rml/r2rml.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { OpcMappingElementComponent } from './opc/subcomponents/opc-mapping-element.component';
 import { OpcMappingService } from './opc/opc-mapping.service';
 
@@ -16,7 +16,8 @@ import { OpcMappingService } from './opc/opc-mapping.service';
   imports: [
     CommonModule,
     MappingRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     MappingComponent,
