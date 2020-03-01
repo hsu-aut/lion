365884cee1b9b945072749af010ec4e64466d5b5
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-03-01 16:48:26 +0100

    Set a higher size limit so that large opc models can be sent

--- lion_backEnd/app.js
+++ lion_backEnd/app.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit:'2mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(cookieParser());
