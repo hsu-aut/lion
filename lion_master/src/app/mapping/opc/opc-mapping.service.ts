d6de9854d1f2517337947d16e10660c6f60b192f
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-03-02 16:09:40 +0100

    Minor cleanups

--- lion_master/src/app/mapping/opc/opc-mapping.service.ts
+++ lion_master/src/app/mapping/opc/opc-mapping.service.ts
@@ -14,7 +14,7 @@ constructor(private httpClient: HttpClient) {}
     opcRoute = '/lion_BE/opc-ua'
 
 
-    crawlServer(serverInfo: ServerInfo): Observable<any> {
+    crawlServer(serverInfo: OpcUaServerInfo): Observable<any> {
         return this.httpClient.post(`${this.opcRoute}/crawl-server`, serverInfo);
     }
 
@@ -81,30 +81,13 @@ constructor(private httpClient: HttpClient) {}
     /**
      * Creates the mapping string for the complete list of nodesToMap
      */
-    createMapping(): Observable<string> {
-        // console.log(this.nodesToMap);
-        // // TODO: Currently setting only the simple type "Node"
-        // // --> We have to differ between different types of nodes (e.g. Method, Variable, ...)
-        // // --> Node types can be differentiated by checking the NodeClass. Currently only the nodeClass od the root node is in the parsed model.
-        // // --> Seems to be a problem with the parser
-        // let queryString = "";
-        // this.nodesToMap.forEach(node => {
-        //     const keys = Object.keys(node);
-        //     // create an individual for this node:
-        //     queryString += `example:${node["browseName"]} rdf:type opc:node.\n`
-
-        //     keys.forEach(key => {
-        //         if(Array.isArray(node[key])) {
-        //             queryString += this.createObjectProperty(key, node);                        // add object properties
-        //         } else {
-        //             queryString += `example:${node["browseName"]} opc:${key} "${node[key]}".\n`   // add data properties
-        //         }
-        //     });
-        // });
-        // console.log(queryString);
-        console.log("creating mappings");
-
-        return this.httpClient.post(`${this.opcRoute}/mappings`, this.nodesToMap) as Observable<string>;
+    createMapping(serverInfo: OpcUaServerInfo): Observable<string> {
+        // Add server info to nodes that should be mapped
+        const dataToMap = {
+            serverInfo: serverInfo,
+            nodesToMap: this.nodesToMap
+        }
+        return this.httpClient.post(`${this.opcRoute}/mappings`, dataToMap) as Observable<string>;
     }
 
 
@@ -129,7 +112,7 @@ constructor(private httpClient: HttpClient) {}
 }
 
 
-export interface ServerInfo {
+export interface OpcUaServerInfo {
     endpointUrl: string,
     securityPolicy: string,
     messageSecurityMode: string,
