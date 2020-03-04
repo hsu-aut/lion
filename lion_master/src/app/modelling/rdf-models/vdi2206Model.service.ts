8e3c77b4f9a04a7d0db357306f145f519b622ea3
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-03-04 09:04:07 +0100

    Added typing to be able to use function return more easily in opc-vdi2206 connector component

--- lion_master/src/app/modelling/rdf-models/vdi2206Model.service.ts
+++ lion_master/src/app/modelling/rdf-models/vdi2206Model.service.ts
@@ -4,6 +4,7 @@ import { QueriesService } from '../../shared/services/backEnd/queries.service';
 import { GraphOperationsService } from '../../shared/services/backEnd/graphOperations.service';
 import { DataLoaderService } from '../../shared/services/dataLoader.service';
 import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -29,7 +30,7 @@ constructor(
   private nameService: PrefixesService,
   private loadingScreenService: DataLoaderService,
   private graphs: GraphOperationsService
-) { 
+) {
 
   this.initializeVDI2206();
 
@@ -78,13 +79,13 @@ public initializeVDI2206(){
   });
 }
 
-public loadLIST_OF_SYSTEMS(){
+public loadLIST_OF_SYSTEMS(): Observable<Array<string>>{
   this.loadingScreenService.startLoading();
-  return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.SPARQL_SELECT_ALL_SYSTEMS, 0);
+  return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.SPARQL_SELECT_ALL_SYSTEMS, 0) as Observable<Array<string>>;
 }
-public loadLIST_OF_MODULES(){
+public loadLIST_OF_MODULES(): Observable<Array<string>>{
   this.loadingScreenService.startLoading();
-  return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.SPARQL_SELECT_ALL_MODULES, 0);
+  return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.SPARQL_SELECT_ALL_MODULES, 0)  as Observable<Array<string>>;
 }
 public loadLIST_OF_COMPONENTS(){
   this.loadingScreenService.startLoading();
@@ -119,19 +120,19 @@ public loadTABLE_STRUCTUAL_INFO_BY_INHERITANCE_BY_COM(){
   return this.query.SPARQL_SELECT_TABLE(this.vdi2206Data.SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_COM);
 }
 
-public loadLIST_OF_PREDICATES_BY_DOMAIN(owlClass){ 
+public loadLIST_OF_PREDICATES_BY_DOMAIN(owlClass){
   owlClass = this.nameService.parseToIRI(owlClass);
   return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.selectPredicateByDomain(owlClass), 0);
 }
-public loadLIST_OF_CLASSES_BY_RANGE(predicate){ 
+public loadLIST_OF_CLASSES_BY_RANGE(predicate){
   predicate = this.nameService.parseToIRI(predicate);
   return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.selectClassByRange(predicate), 0);
 }
-public loadLIST_OF_CLASS_MEMBERSHIP(individual){ 
+public loadLIST_OF_CLASS_MEMBERSHIP(individual){
   individual = this.nameService.parseToIRI(individual);
   return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.selectClass(individual), 0);
 }
-public loadLIST_OF_INDIVIDUALS_BY_CLASS(Class){ 
+public loadLIST_OF_INDIVIDUALS_BY_CLASS(Class){
   Class = this.nameService.parseToIRI(Class);
   return this.query.SPARQL_SELECT_LIST(this.vdi2206Data.selectIndividualByClass(Class), 0);
 }
@@ -248,7 +249,7 @@ export class VDI2206DATA {
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-SELECT ?System WHERE { 
+SELECT ?System WHERE {
 ?System a VDI2206:System.
 }
 `;
@@ -257,7 +258,7 @@ public SPARQL_SELECT_ALL_MODULES = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-SELECT ?Module WHERE { 
+SELECT ?Module WHERE {
 ?Module a VDI2206:Module.
 }
 `;
@@ -266,7 +267,7 @@ public SPARQL_SELECT_ALL_COMPONENTS = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-SELECT ?Component WHERE { 
+SELECT ?Component WHERE {
 ?Component a VDI2206:Component.
 }
 `;
@@ -284,7 +285,7 @@ FILTER(STRSTARTS(STR(?type), "http://www.hsu-ifa.de/ontologies/VDI2206#"))
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?System ?consistsOfEntity ?EntityType WHERE { 
+SELECT ?System ?consistsOfEntity ?EntityType WHERE {
 ?System a VDI2206:System.
   OPTIONAL {?System VDI2206:consistsOf ?consistsOfEntity. ?consistsOfEntity rdf:type ?EntityType. VALUES ?EntityType {VDI2206:System VDI2206:Module VDI2206:Component}}
 }
@@ -296,7 +297,7 @@ public SPARQL_SELECT_STRUCTUAL_INFO_BY_CONTAINMENT_BY_MOD = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?Module ?consistsOfEntity ?EntityType WHERE { 
+SELECT ?Module ?consistsOfEntity ?EntityType WHERE {
 ?Module a VDI2206:Module.
   OPTIONAL {?Module VDI2206:consistsOf ?consistsOfEntity. ?consistsOfEntity rdf:type ?EntityType. VALUES ?EntityType {VDI2206:Module VDI2206:Component}}
 }
@@ -306,7 +307,7 @@ public SPARQL_SELECT_STRUCTUAL_INFO_BY_CONTAINMENT_BY_COM = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?Component WHERE { 
+SELECT ?Component WHERE {
 ?Component a VDI2206:Component.
 }
 `;
@@ -316,7 +317,7 @@ public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_SYS = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?System ?childEntity ?childEntityType WHERE { 
+SELECT ?System ?childEntity ?childEntityType WHERE {
 ?System a VDI2206:System.
   OPTIONAL {?System VDI2206:hasChild ?childEntity. ?childEntity rdf:type ?childEntityType. VALUES ?childEntityType {VDI2206:System VDI2206:Module VDI2206:Component}}
 }
@@ -325,7 +326,7 @@ public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_MOD = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?Module ?childEntity ?childEntityType WHERE { 
+SELECT ?Module ?childEntity ?childEntityType WHERE {
 ?Module a VDI2206:Module.
   OPTIONAL {?Module VDI2206:hasChild ?childEntity. ?childEntity rdf:type ?childEntityType. VALUES ?childEntityType {VDI2206:Module VDI2206:Component}}
 }
@@ -334,7 +335,7 @@ public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_COM = `
 PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-SELECT ?Component ?childEntity ?childEntityType WHERE { 
+SELECT ?Component ?childEntity ?childEntityType WHERE {
 ?Component a VDI2206:Component.
   OPTIONAL {?Component VDI2206:hasChild ?childEntity. ?childEntity rdf:type ?childEntityType. VALUES ?childEntityType {VDI2206:Component}}
 }
@@ -360,13 +361,13 @@ SELECT ?Component ?childEntity ?childEntityType WHERE {
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  SELECT ?ObjectProperty WHERE { 
+  SELECT ?ObjectProperty WHERE {
       ?ObjectProperty rdfs:domain ?a.
       # optionally if the range is a blank node not changes required
       OPTIONAL {    	?a owl:unionOf ?c.
                       ?c rdf:rest* ?e.
                       ?e rdf:first ?g.}
-      # in case the range is a blank node, use the rdf:first as return    
+      # in case the range is a blank node, use the rdf:first as return
       BIND(IF(isBlank(?a),?g,?a) AS ?Property)
       # filter for class
       FILTER(?Property = IRI("${owlClass}"))
@@ -379,13 +380,13 @@ SELECT ?Component ?childEntity ?childEntityType WHERE {
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-  SELECT DISTINCT ?Class WHERE { 
+  SELECT DISTINCT ?Class WHERE {
       ?ObjectProperty rdfs:range ?a.
       # optionally if the range is a blank node not changes required
       OPTIONAL {    	?a owl:unionOf ?c.
                       ?c rdf:rest* ?e.
                       ?e rdf:first ?g.}
-      # in case the range is a blank node, use the rdf:first as return    
+      # in case the range is a blank node, use the rdf:first as return
       BIND(IF(isBlank(?a),?g,?a) AS ?Class)
       # filter for class
       FILTER(?ObjectProperty = IRI("${predicate}"))
@@ -422,7 +423,7 @@ export class VDI2206INSERT {
   public createEntity(graph: tripel, activeGraph) {
 
       var insertString = `
-      INSERT { 
+      INSERT {
         GRAPH <${activeGraph}>{
           ?subject ?predicate ?object;
           a owl:NamedIndividual.}
