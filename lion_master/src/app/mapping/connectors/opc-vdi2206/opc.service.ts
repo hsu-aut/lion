e006e6ccf75856960151daeaeda28236bd809c66
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-03-04 09:03:10 +0100

    Added functions to create and load OPC-VDI2206 connections

--- lion_master/src/app/mapping/connectors/opc-vdi2206/opc.service.ts
+++ lion_master/src/app/mapping/connectors/opc-vdi2206/opc.service.ts
@@ -44,13 +44,45 @@ export class OpcService {
         return this.queryService.SPARQL_UPDATE(query);
     }
 
-
     loadVariableAnd61360Connections() {
         const query = `PREFIX lf: <http://lionFacts#>
         PREFIX OpcUa: <http://www.hsu-ifa.de/ontologies/OpcUa#>
         PREFIX DINEN61360: <http://www.hsu-ifa.de/ontologies/DINEN61360#>
         SELECT ?instanceDescription ?opcVariable {
             ?instanceDescription DINEN61360:hasOntologicalValue ?opcVariable.
+        }`;
+        return this.queryService.SPARQL_SELECT_TABLE(query);
+    }
+
+
+    createOpcVdi2206Connection(systemOrModule: string, opcUaServer: string) {
+        const query = `PREFIX lf: <http://lionFacts#>
+        PREFIX OpcUa: <http://www.hsu-ifa.de/ontologies/OpcUa#>
+        PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+        INSERT DATA {
+            <${this.prefixService.addOrParseNamespace(systemOrModule)}> OpcUa:hasOpcUaServer <${opcUaServer}>.
+        }`
+
+        console.log(query);
+
+        return this.queryService.SPARQL_UPDATE(query);
+    }
+
+
+    loadServerAndVdi2206Connections() {
+        const query = `PREFIX lf: <http://lionFacts#>
+        PREFIX OpcUa: <http://www.hsu-ifa.de/ontologies/OpcUa#>
+        PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+        SELECT ?system ?module ?opcUaServer {
+            {
+                ?system a VDI2206:System.
+                ?system OpcUa:hasOpcUaServer ?opcUaServer.
+            }
+            UNION
+            {
+                ?module a VDI2206:Module.
+                ?module OpcUa:hasOpcUaServer ?opcUaServer.
+            }
         }`;
         return this.queryService.SPARQL_SELECT_TABLE(query);
     }
