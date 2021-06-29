95e20b494b44ce67e5121bfa2a76c67be12bb30a
LasseBeers <lasse_beers@hotmail.de>
2021-06-29 16:40:42 +0200

    Revision of "Queries". NOT DONE

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FpbModule } from './routes/fpb/fpb.module';
+import { QuerieController } from './routes/queries/queries.controller';
+import { QueriesModule } from './routes/queries/queries.module';
 import { StepModule } from './routes/step/step.module';
 
 @Module({
 	imports: [
 		FpbModule,
-		StepModule
+		StepModule,
+		QueriesModule
 	],
 	controllers: [AppController],
 	providers: [AppService],
