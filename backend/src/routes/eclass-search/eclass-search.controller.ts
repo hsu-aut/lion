ed292fffbd870e5d345e9ee2681adddbd3540924
Weigand <maximilian.weigand@hsu-hh.de>
2021-07-03 23:49:11 +0200

    added eclass-search module/controller/servce

---
+++ backend/src/routes/eclass-search/eclass-search.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { EclassSearchService } from './eclass-search.service';
+
+@Controller('lion_BE/eclassSearch')
+export class EclassSearchController {
+	constructor(private eclassSearchService: EclassSearchService) {}
+
+  @Get('connect')
+	connectDb(): void {
+		this.eclassSearchService.connectDb();
+	}
+
+  @Get('disconnect')
+  disconnectDb(): void {
+  	this.eclassSearchService.disconnectDb();
+  }
+
+  @Get('list')
+  getPropertiesList(@Query('prop') prop, @Query('via') via): Promise<any> {
+  	if (via == 'db') {
+  		// do if access via mysql db is requested
+  		try {
+  			return this.eclassSearchService.getPropertiesByNamefromDb(prop);
+  		} catch (error) {
+  			console.error(error + 'cannot get property from database');
+  		}
+  	} else if (via == 'ws') {
+  		// do if access via webservice is requested
+  		try {
+  			console.log("eclass webservice not yet implemented :'(");
+  		} catch (error) {
+  			console.error(error + 'cannot get property from webservice');
+  		}
+  	} else {
+  		// do if access not properly defined
+  		console.error('parameter not defined:' + via);
+  	}
+  }
+}
