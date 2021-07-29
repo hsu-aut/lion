992b5f564b31675c674d4c8117845608ca29d39b
JReisberg <jannik.reisberg@hsu-hh.de>
2021-07-29 17:06:58 +0200

    Fix Comment

--- backend/src/routes/repositories/repositories.controller.ts
+++ backend/src/routes/repositories/repositories.controller.ts
@@ -4,12 +4,42 @@ import { Controller, Delete, Get, Post} from '@nestjs/common';
 export class RepositoriesController {
 constructor() {}
 
+ar express = require('express');
+var router = express.Router();
+var url = require('url');
+
+var GDB_TBOX = require('../GRAPH_DB_REQUESTS/tboxOperations.requests')
+var GDB_REPO = require('../GRAPH_DB_REQUESTS/repositoryOperations.requests')
+var gdbConfig = require('../GRAPH_DB_REQUESTS/GDBconfigurator')
+
+// const curl = new (require('curl-request'))();
+const axios = new (require('axios'))();
+const FormData = require('form-data');
+const fs = require('fs');
+
 /**
  * GET list of repositories 
  */
 
 @Get()
-getListOfRepositories():void {
+getListOfRepositories():void, function (req, res, next) {
+    GDB_REPO.GET_REPOSITORIES().then(function (response) {
+
+        if (response.status == 200) {
+            res.status(200);
+            res.end(response.data);
+        } else {
+            res.status(500);
+            res.end();
+        }
+
+    })
+        .catch(function (error) {
+            console.log(error);
+            res.status(500);
+            res.end();
+        });
+
 }
 
 /**
