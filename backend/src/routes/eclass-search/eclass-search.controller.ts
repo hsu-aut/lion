0ccd4e9665c0ce5d6a49843a9cc369269f450de1
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2021-08-10 17:57:58 +0200

    Formatted code to have consistent indentation

--- backend/src/routes/eclass-search/eclass-search.controller.ts
+++ backend/src/routes/eclass-search/eclass-search.controller.ts
@@ -3,38 +3,38 @@ import { EclassSearchService } from './eclass-search.service';
 
 @Controller('lion_BE/eclassSearch')
 export class EclassSearchController {
-	constructor(private eclassSearchService: EclassSearchService) {}
+	constructor(private eclassSearchService: EclassSearchService) { }
 
-  @Get('connect')
+	@Get('connect')
 	connectDb(): void {
 		this.eclassSearchService.connectDb();
 	}
 
-  @Get('disconnect')
-  disconnectDb(): void {
-  	this.eclassSearchService.disconnectDb();
-  }
+	@Get('disconnect')
+	disconnectDb(): void {
+		this.eclassSearchService.disconnectDb();
+	}
 
-  @Get('list')
-  getPropertiesList(@Query('prop') prop, @Query('via') via): Promise<any> {
-  	if (via == 'db' || via == null) {
-  		// do if access via mysql db is requested 
-		// default, also chosse this option if no parameter 'via' was defined  
-		try {
-  			return this.eclassSearchService.getPropertiesByNamefromDb(prop);
-  		} catch (error) {
-  			console.error(error + 'cannot get property from database');
-  		}
-  	} else if (via == 'ws') {
-  		// do if access via webservice is requested
-  		try {
-  			console.log("eclass webservice not yet implemented :'(");
-  		} catch (error) {
-  			console.error(error + 'cannot get property from webservice');
-  		}
-  	} else {
-  		// do if access not properly defined
-  		console.error('parameter not defined:' + via);
-  	}
-  }
+	@Get('list')
+	getPropertiesList(@Query('prop') prop, @Query('via') via): Promise<any> {
+		if (via == 'db' || via == null) {
+			// do if access via mysql db is requested 
+			// default, also chosse this option if no parameter 'via' was defined  
+			try {
+				return this.eclassSearchService.getPropertiesByNamefromDb(prop);
+			} catch (error) {
+				console.error(error + 'cannot get property from database');
+			}
+		} else if (via == 'ws') {
+			// do if access via webservice is requested
+			try {
+				console.log("eclass webservice not yet implemented :'(");
+			} catch (error) {
+				console.error(error + 'cannot get property from webservice');
+			}
+		} else {
+			// do if access not properly defined
+			console.error('parameter not defined:' + via);
+		}
+	}
 }
--- backend/src/routes/eclass-search/eclass-search.service.ts
+++ backend/src/routes/eclass-search/eclass-search.service.ts
@@ -6,59 +6,59 @@ import { ConnectionOptions } from 'mysql2';
 
 @Injectable()
 export class EclassSearchService {
-  private pool: Pool;
+	private pool: Pool;
 
-  async connectDb(): Promise<void> {
-  	try {
-  		//read db config from json file
-  		const dbConfig: ConnectionOptions = JSON.parse(readFileSync('./eclass-db-config.json', 'utf-8'));
-  		//establish pool connection with db config
-  		this.pool = createPool(dbConfig);
-  	} catch (err) {
-  		console.error('db connection error: ' + err);
-  	}
-  }
+	async connectDb(): Promise<void> {
+		try {
+			//read db config from json file
+			const dbConfig: ConnectionOptions = JSON.parse(readFileSync('./eclass-db-config.json', 'utf-8'));
+			//establish pool connection with db config
+			this.pool = createPool(dbConfig);
+		} catch (err) {
+			console.error('db connection error: ' + err);
+		}
+	}
 
-  //disconnect pool
-  async disconnectDb(): Promise<void> {
-  	try {
-  		if (this.pool) {
-  			// dsiconnect pool
-  			this.pool.end();
-  			this.pool = null;
-  		} else {
-  			// throw error if no pool is connected
-  			throw new Error('no connection established');
-  		}
-  	} catch (err) {
-  		console.error('db disconnection error: ' + err);
-  	}
-  }
-  
-  //execute query
-  //todo: define type of returned values
-  
-  async getPropertiesByNamefromDb(propName: string): Promise<any> {
-  	try {
-  		// query string
-  	  const queryString =
-      `SELECT p.Identifier, p.VersionNumber, p.RevisionNumber, p.PreferredName, p.ShortName, p.Definition, p.DataType, uom.DINNotation 
+	//disconnect pool
+	async disconnectDb(): Promise<void> {
+		try {
+			if (this.pool) {
+				// dsiconnect pool
+				this.pool.end();
+				this.pool = null;
+			} else {
+				// throw error if no pool is connected
+				throw new Error('no connection established');
+			}
+		} catch (err) {
+			console.error('db disconnection error: ' + err);
+		}
+	}
+
+	//execute query
+	//todo: define type of returned values
+
+	async getPropertiesByNamefromDb(propName: string): Promise<any> {
+		try {
+			// query string
+			const queryString =
+				`SELECT p.Identifier, p.VersionNumber, p.RevisionNumber, p.PreferredName, p.ShortName, p.Definition, p.DataType, uom.DINNotation 
       FROM eclassproperty AS p
       LEFT OUTER JOIN eclassunit AS uom ON p.IrdiUN = uom.IrdiUN
       WHERE PreferredName LIKE '%${propName}%'`;
 
-  		if (this.pool) {
-  			// db.query returns [rows, fields] - read rows only
-  			const [rows] = await this.pool.query(queryString);
-  			return rows;
-  		} else {
-  			// throw error if no pool is connected
-  			throw new Error('no connection established');
-  		}
-  	} catch (err) {
-  		console.error('query error: ' + err);
-  	}
-  }
+			if (this.pool) {
+				// db.query returns [rows, fields] - read rows only
+				const [rows] = await this.pool.query(queryString);
+				return rows;
+			} else {
+				// throw error if no pool is connected
+				throw new Error('no connection established');
+			}
+		} catch (err) {
+			console.error('query error: ' + err);
+		}
+	}
 }
 
 // old code: manual error handling
