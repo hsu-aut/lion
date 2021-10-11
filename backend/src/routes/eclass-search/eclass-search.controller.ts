d7eb7dd9120efbff5e8ee76fa8a70732451961b1
Weigand <maximilian.weigand@hsu-hh.de>
2021-10-11 11:32:44 +0200

    added route to load webservice certificate moddified types (Promise<EclassProperty>)

--- backend/src/routes/eclass-search/eclass-search.controller.ts
+++ backend/src/routes/eclass-search/eclass-search.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { EclassProperty } from './eclass-property.interface';  //todo change to @shared folder
 import { EclassSearchService } from './eclass-search.service';
 
 @Controller('lion_BE/eclassSearch')
@@ -7,28 +8,33 @@ export class EclassSearchController {
 
 	@Get('connect')
 	connectDb(): void {
-		this.eclassSearchService.connectDb();
+		this.eclassSearchService.connectDB();
 	}
 
 	@Get('disconnect')
 	disconnectDb(): void {
-		this.eclassSearchService.disconnectDb();
+		this.eclassSearchService.disconnectDB();
+	}
+
+	@Get('configWS')
+	createRequestConfig(): void {
+		this.eclassSearchService.createRequestConfig();
 	}
 
 	@Get('list')
-	getPropertiesList(@Query('prop') prop, @Query('via') via): Promise<any> {
+	getPropertiesList(@Query('prop') prop: string, @Query('via') via: string): Promise<EclassProperty[]> {
 		if (via == 'db' || via == null) {
 			// do if access via mysql db is requested 
 			// default, also chosse this option if no parameter 'via' was defined  
 			try {
-				return this.eclassSearchService.getPropertiesByNamefromDb(prop);
+				return this.eclassSearchService.getPropertiesByNameFromDB(prop);
 			} catch (error) {
 				console.error(error + 'cannot get property from database');
 			}
 		} else if (via == 'ws') {
 			// do if access via webservice is requested
 			try {
-				console.log("eclass webservice not yet implemented :'(");
+				return this.eclassSearchService.getPropertiesByNameFromWebService(prop);
 			} catch (error) {
 				console.error(error + 'cannot get property from webservice');
 			}
