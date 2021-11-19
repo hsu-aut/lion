e2855faaaaef2d1240f6f885dc7e0e7dd309f729
Westermann <tom.westermann@hsu-hh.de>
2021-11-19 16:45:17 +0100

    rewrote opc functionality in typescript

--- backend/src/routes/opc-ua/opc-ua.controller.ts
+++ backend/src/routes/opc-ua/opc-ua.controller.ts
@@ -1,43 +1,84 @@
-import { Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors, Body } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put, UploadedFile, UseInterceptors, Body , Res, Req} from '@nestjs/common';
 // import { FileInterceptor } from '@nestjs/platform-express';
 // import * as fs from "fs";
+import { Response } from 'express';
 import { OpcUaService } from './opc-ua.service';
+import { OpcUaClientCreator } from './opc-ua-client.service';
+import { OpcUaMappingCreator } from './opc-ua-mapper.service';
+import { SparqlService } from '../../shared-services/sparql.service' ;
+// import { OpcNode } from './subcomponents/opc-mapping-element.component';
+
+
+export class ServerInfo{
+	endpointUrl: string;
+	securityPolicy: string;
+	messageSecurityMode: string;
+	userName: string;
+	password: string;
+}
+
+export class DataToMap{
+	// nodesToMap: Array<OpcNode>
+	serverInfo: ServerInfo
+	serverIp: string
+	opcPrefix: string
+	repository: string // referenced, but not assigned
+	opc: Opc
+}
+
+export class Opc{
+	serverInfo: ServerInfo
+	nodesToMap: Array<OpcNode>
+}
+
+export class OpcNode{
+	uuid: string
+	nodeClass: string
+	nodeId: string
+}
 
 @Controller('/lion_BE/opc-ua')
 export class OpcUaController {
 
-	constructor(private opcuaService: OpcUaService) {}
+	constructor(
+		private opcuaService: OpcUaService ,
+		private sparqlService: SparqlService) {}
 
-    /* 
-     * crawl a server 
-    */
-    @Post('/crawl-server')
-	crawlServer(@Body() serverInfo: ServerInfo): void {
+	/* 
+	 * crawl a server 
+	*/
+	@Post('/crawl-server')
+	crawlServer(@Body() serverInfo: ServerInfo, @Res() response: Response): void {
 		console.log('trying to crawl server.');
-        
-		connectAndCrawl();
-
-		let crawlResult;
-		async function connectAndCrawl() {
-			const opcUaClientCreator = new OpcUaClientCreator(serverInfo);
-			const opcUaClient = await opcUaClientCreator.createClient();
-			await opcUaClient.connect(serverInfo.endpointUrl);
-			const session = await opcUaClient.createSession();
-            
-			const nodeCrawler = new NodeCrawler(session);
-			nodeCrawler.read("RootFolder").then(crawlResult => {
-				res.status(200).json(crawlResult);
-			});
-		}
+		this.opcuaService.connectAndCrawl(serverInfo, response);
+		// connectAndCrawl();
+		
+		// let crawlResult;
 	}
 
-}
+	@Post('/mappings') 
+	updateMappings(@Body() dataToMap: DataToMap): void {
+		console.log('Updating Mappings...');
+		console.log('dataToMap:');
+		console.log(dataToMap);
 
-class ServerInfo{
-    endpointUrl: string;
-    securityPolicy: string;
-    messageSecurityMode: string;
-    username: string;
-    password: string;
+		console.log('nodesToMap:');
+		console.log(dataToMap.opc.nodesToMap);
+		// figure out where to put: function(req, res)
+		// const repository = dataToMap.repository;
+
+		const opcUaMapper = new OpcUaMappingCreator(dataToMap);
+		const mapping = opcUaMapper.createMapping();
+
+		console.log(`Mapping Query: ${mapping}`);
+		this.sparqlService.update(mapping) ;
+
+		// sparqlUpdate(mapping, repository) { }: .then(queryRes => {
+		// 	res.status(200).json(queryRes);
+		// }).catch(err => {
+		// 	res.status(500).json(err);
+		// })
+	}
+
 }
 
--- backend/src/routes/opc-ua/opc-ua.service.ts
+++ backend/src/routes/opc-ua/opc-ua.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Res } from '@nestjs/common';
+import { Response } from 'express';
+// import {  } from '@nestjs/platform-express';
+import { NodeCrawler } from 'node-opcua';
+import { OpcUaClientCreator} from './opc-ua-client.service';
+import { ServerInfo } from './opc-ua.controller';
 // import * as fs from "fs/promises";
 
 @Injectable()
 export class OpcUaService {
-    
+	async connectAndCrawl(serverInfo: ServerInfo, @Res() response: Response ): Promise<void> {
+		const opcUaClientCreator = new OpcUaClientCreator(serverInfo);
+		const opcUaClient = await opcUaClientCreator.createClient();
+		await opcUaClient.connect(serverInfo.endpointUrl);
+		const session = await opcUaClient.createSession();
+
+		const nodeCrawler = new NodeCrawler(session);
+		// console.log(session);
+		nodeCrawler.read("RootFolder").then(crawlResult => {
+			console.log(crawlResult);
+			response.status(200).json(crawlResult); 
+			// res.status(200).json(crawlResult);
+		});
+	}
 }
