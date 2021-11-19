31feb446107cf8c628dbbe90602513a5cedded40
Westermann <tom.westermann@hsu-hh.de>
2021-11-19 16:45:17 +0100

    rewrote opc functionality in typescript

--- backend/src/routes/opc-ua/opc-ua.module.ts
+++ backend/src/routes/opc-ua/opc-ua.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { OpcUaController } from './opc-ua.controller';
 // import { MulterModule } from '@nestjs/platform-express';
 import { OpcUaService } from './opc-ua.service';
+import { OpcUaClientCreator } from './opc-ua-client.service';
+import { OpcUaMappingCreator } from './opc-ua-mapper.service';
 
 @Module({
 	imports: [],
-	controllers: [
-		OpcUaController
-	],
+	controllers: [OpcUaController],
 	providers: [
-		OpcUaService
+		OpcUaService,
+		// OpcUaClientCreator,
+		// OpcUaMappingCreator
 	],
 })
 export class OpcUaModule {
