ccdcce62bd90d0534955d63d8a2a86dca5d4155f
Weigand <maximilian.weigand@hsu-hh.de>
2022-02-24 17:41:06 +0100

    created new backend service to handle dinen61360 related communication with grapdb

---
+++ backend/src/routes/dinen61360/dinen61360.module.ts
@@ -0,0 +1,10 @@
+import { HttpModule, Module } from '@nestjs/common';
+import { DINEN61360Controller } from './dinen61360.controller';
+import { DINEN61360Service } from './dinen61360.service';
+
+@Module({
+	imports: [HttpModule], // todo: remove (probably)
+	controllers: [DINEN61360Controller],
+	providers: [DINEN61360Service],
+})
+export class DINEN61360Module {}
