c1f5b09bb2e58bd24b7c1df3c5788b08051efd0c
Westermann <tom.westermann@hsu-hh.de>
2022-02-24 17:11:15 +0100

    increased file limit for json files to 50mb

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+
+	app.use(json({ limit: '50mb' }));
+	app.use(urlencoded({ extended: true, limit: '50mb' }));
+	app.enableCors();
 	await app.listen(3000);
 }
 bootstrap();
