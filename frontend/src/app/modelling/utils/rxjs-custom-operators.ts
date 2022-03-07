e9392a90a9808509c3d3b69281dd88818f4db2e3
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2022-03-07 13:40:02 +0100

    Adds two RxJS pipe operators to convert SparqlResponse

---
+++ frontend/src/app/modelling/utils/rxjs-custom-operators.ts
@@ -0,0 +1,55 @@
+import { SparqlResponse } from "@shared/interfaces/sparql/SparqlResponse";
+import { map, Observable } from "rxjs";
+
+/**
+ * Extracts all values of one variable from a SparqlResponse. In case only one variable is present in the SparqlResponse, no variableName is
+ * needed and the values of this one variable are returned.
+ * @param source$ The source observable (a default / raw SparqlResponse)
+ * @returns An array of values for the given variableName. In case there is just one variable, the list of values for this variable
+ */
+export function toSparqlVariableList(source$: Observable<SparqlResponse>, variableName?: string): Observable<Array<string | number>> {
+    return source$.pipe(map(v => {
+        const vars = v.head.vars;
+        const bindings = v.results.bindings;
+        // grab all values of the given variableName
+        if (variableName != null) {
+            return bindings.map(bindingObj => bindingObj[variableName].value);
+        }
+        // if there is just one variabe, grab this variable's values
+        if (vars.length == 1) {
+            const onlyVariableName = vars[0];
+            return bindings.map(bindingObj => bindingObj[onlyVariableName].value);
+        }
+        // else (no name given and more than one variables present), throw error
+        throw "Trying to create a list of one variable but the query contains more than one. Please specify a variable.";
+    }));
+}
+
+/**
+ * Converts a source observable of type SparqlResponse to a table-like array where all non existing values of the raw response were filled with empty strings.
+ * @param source$ The source observable (a default / raw) SparqlResponse
+ * @returns An array of values in which each array contains an object that has the variables  of the SparqlResponse as keys
+ */
+export function toSparqlTable(source$: Observable<SparqlResponse>): Observable<Array<Record<string, string | number>>> {
+    return source$.pipe(map(sparqlResponse => {
+        const tableArray = new Array<Record<string, string | number>>();
+        const vars = sparqlResponse.head.vars;
+        const bindings = sparqlResponse.results.bindings;
+        bindings.forEach(b => {
+            const entry: Record<string, string | number> = {};
+            let val;
+            vars.forEach(v => {
+                try {
+                    val = b[v].value;
+                } catch {
+                    val = "";
+                }
+                entry[v] = val;
+            });
+            tableArray.push(entry);
+        });
+        // // v.results.bindings.map(binding => binding)
+        return tableArray;
+
+    }));
+}
