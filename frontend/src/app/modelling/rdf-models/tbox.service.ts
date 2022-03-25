7c256440afb4f9c6e221b4587d37df1f9deef6e1
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2022-03-25 19:54:30 +0100

    Added a frontend tbox-service that should be used for all the stuff thats independent of an ODP

---
+++ frontend/src/app/modelling/rdf-models/tbox.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * A service that offers basic interactions on T-Box level such as getting all classes (within a certain namespace)
+ */
+@Injectable({
+    providedIn: 'root'
+})
+export class TboxService {
+
+    constructor() { }
+
+}
