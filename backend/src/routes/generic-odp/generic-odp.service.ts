89513d63132566df0472b0357d141fb9999e088d
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2022-12-01 21:34:40 +0100

    added new methods/queries

--- backend/src/routes/generic-odp/generic-odp.service.ts
+++ backend/src/routes/generic-odp/generic-odp.service.ts
@@ -2,20 +2,47 @@ import { Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { SparqlService } from '../../shared-services/sparql.service';
 import { SparqlResponse } from '@shared/models/sparql/SparqlResponse';
-import { SparqlTemplateService } from './sparql-template.service';
+import { GraphOperationService } from '../../shared-services/graph-operation.service';
 
 @Injectable()
 export class GenericOdpService {
 	
 	constructor(
 		private sparqlService: SparqlService,
-		private sparqlTemplateServic: SparqlTemplateService,
+		private graphOperationService: GraphOperationService
 	){}
 
 	// get routes
-	public getAllClasses(): Observable<SparqlResponse> {
-        return this.sparqlService.query(this.sparqlTemplateServic.selectAllClasses());
-	}
+	// TODO figure out if grap has to be specified with SELECT x FROM <http://lionFacts> WHERE y
+    public getAllClasses(): Observable<SparqlResponse> {
+        return this.sparqlService.query(`
+			PREFIX owl: <http://www.w3.org/2002/07/owl#>
+			SELECT ?class
+			WHERE {
+				?class a owl:Class .
+			}
+		`);
+    }
+    public getAllObjectPropertiesForDomainIndividual(individualIri: string): Observable<SparqlResponse> {
+        return this.sparqlService.query(`
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            SELECT ?object_property
+            WHERE {
+                ${individualIri} a ?rdf_type_of_subject .
+                ?object_property a owl:ObjectProperty .
+                ?object_property rdfs:domain ?rdf_type_of_subject .
+            }
+        `);    
+    }
+    public getAllIndividualsOfClass(classIri: string): Observable<SparqlResponse> {
+        return this.sparqlService.query(`
+			SELECT ?individual
+			WHERE {
+				?individual a ${classIri}
+			}
+        `);
+    }
 
 }
 
