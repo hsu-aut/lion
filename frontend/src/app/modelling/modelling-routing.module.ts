15d74cd397ca1a66d1cae275f94c14748e3d8af6
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2022-12-01 20:26:47 +0100

    added generic-odp to modelling routing module

--- frontend/src/app/modelling/modelling-routing.module.ts
+++ frontend/src/app/modelling/modelling-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
             { path: 'isa88', loadChildren:() => import('./isa88/isa88.module').then(m => m.Isa88Module)},
             { path: 'dinen61360', loadChildren:() => import('./dinen61360/dinen61360.module').then(m => m.Dinen61360Module)},
             { path: 'iso22400-2', loadChildren:() => import('./iso224002/iso224002.module').then(m => m.Iso224002Module)},
-
+            { path: 'generic-odp', loadChildren:() => import('./generic-odp/generic-odp.module').then(m => m.GenericOdpModule)},
         ]
     }
 ];
