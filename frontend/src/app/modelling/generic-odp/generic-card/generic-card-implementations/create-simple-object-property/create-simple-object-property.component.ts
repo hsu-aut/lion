7700842b49d7cd8e1a471a405ad8d84f4180b310
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2022-12-11 19:48:12 +0100

    implemented all 4 dropdowns

--- frontend/src/app/modelling/generic-odp/generic-card/generic-card-implementations/create-simple-object-property/create-simple-object-property.component.ts
+++ frontend/src/app/modelling/generic-odp/generic-card/generic-card-implementations/create-simple-object-property/create-simple-object-property.component.ts
@@ -10,13 +10,25 @@ import { GenericCardContentComponent } from '../../generic-card-content.componen
 })
 export class CreateSimpleObjectPropertyComponent extends GenericCardContentComponent implements OnInit {
 
-  // array of all classes for dropdown and total number
-  public allClasses: Array<string>;
-  public nOfClasses: number;   // currently not used
+  // array of all domain individuals and total number
+  public domainIndividuals: Array<string>;
+  public numberOfDomainIndividuals: number; // currently not used
 
-  // array of all individuals of selected class and total number
-  public allIndividualsOfClass: Array<string>;
-  public nOfIndividualsOfClass: number;   // currently not used
+  // array of all possible object properties for domain individual and total number
+  public objectProperties: Array<string>
+  public numberOfObjectProperties: number; // currently not used
+
+  // array of all range individuals of and total number
+  public rangeIndividuals: Array<string>
+  public numberOfRangeIndividuals: number; // currently not used
+
+  // array of all classes for filter dropdown and total number
+  public classes: Array<string>;
+  public numberOfClasses: number;   // currently not used
+
+  // text to display in dropdown ... 
+  // declared as a variable to avoid typos
+  public allIndividualsFilterText: string = "Show all individuals"
 
   // forms
   newObjectPropertyForm1 = this.formBuilder.group({
@@ -24,35 +36,98 @@ export class CreateSimpleObjectPropertyComponent extends GenericCardContentCompo
   })
   newObjectPropertyForm2 = this.formBuilder.group({
     fc2: ["", Validators.required],
-    fc3: ['rdf:type'],
+    fc3: ["", Validators.required],
     fc4: ["", Validators.required],
   })
 
   ngOnInit(): void {
     this.getAllClasses();
+    this.getAllIndividuals();
   }
 
   getAllClasses(): void {
     this.genericOdpModelService.getAllClasses().subscribe((data: Array<string>) =>{
-      this.allClasses = data;
-      this.nOfClasses = data.length;
+      this.classes = data;
+      this.numberOfClasses = data.length;
     }); 
   }
 
+  getAllIndividuals(): void {
+    this.genericOdpModelService.getAllIndividuals().subscribe((data: Array<string>) =>{
+      this.domainIndividuals = data;
+      this.numberOfDomainIndividuals = data.length;
+    });
+  }
+
   /**
-   * executed when class is selected, all matching individuals are selected for domain individual dropdown
+   * executed when class is selected
+   * all matching domain individuals are requested for dropdown
    */
-  onClassSelection(): void {
-    // get iri from form
+  onClassFilterSelection(): void {
+    // get class iri from form
     const classIri: string = this.newObjectPropertyForm1.controls['fc1'].value;
-    if ( classIri==null || classIri==undefined || classIri=="") { 
-      return; 
+    // delete current entries
+    this.domainIndividuals = [];
+    this.numberOfDomainIndividuals = 0; 
+    this.objectProperties = [];
+    this.numberOfObjectProperties = 0;
+    this.rangeIndividuals = [];
+    this.numberOfRangeIndividuals = 0; 
+    // request new data  
+    if (classIri==this.allIndividualsFilterText) {
+      // request all individuals
+      this.getAllIndividuals();
+    } else {
+      // request matching individuals
+      this.genericOdpModelService.getAllIndividualsOfClass(classIri).subscribe((data: Array<string>) =>{
+        this.domainIndividuals = data;
+        this.numberOfDomainIndividuals = data.length;
+      });
     }
-    // request matching individuals
-    this.genericOdpModelService.getAllIndividualsOfClass(classIri).subscribe((data: Array<string>) =>{
-      this.allIndividualsOfClass = data;
-      this.nOfIndividualsOfClass = data.length;
-    }); 
+  }
+
+  /**
+   * executed when domain idividual is selected
+   * all matching object properties are requested for dropdown
+   */
+  onDomainIndividualSelection(): void {
+    // get individual iri from form
+    const individualIri: string = this.newObjectPropertyForm2.controls['fc2'].value;
+    // delete current entries
+    this.objectProperties = [];
+    this.numberOfObjectProperties = 0;
+    this.rangeIndividuals = [];
+    this.numberOfRangeIndividuals = 0;       
+    // request matching object properties
+    this.genericOdpModelService.getAllObjectPropertiesForDomainIndividual(individualIri).subscribe((data: Array<string>) =>{
+      this.objectProperties = data;
+      this.numberOfObjectProperties = data.length;
+    });
+  }
+
+  /**
+   * executed when object property is selected
+   * all matching range individuals are requested for dropdown
+   */
+  onObjectPropertySelection(): void {
+    // get object property iri from form
+    const objectPropertyIri: string = this.newObjectPropertyForm2.controls['fc3'].value;
+    // delete current entries
+    this.rangeIndividuals = [];
+    this.numberOfRangeIndividuals = 0;   
+    // request matching range individuals
+    this.genericOdpModelService.getAllRangeIndividualsForObjectProperty(objectPropertyIri).subscribe((data: Array<string>) =>{
+      this.rangeIndividuals = data;
+      this.numberOfRangeIndividuals = data.length;
+    });
+  }
+
+  /**
+   * executed when range idividual is selected
+   * x
+   */
+  onRangeIndividualSelection(): void {
+    return;
   }
 
 }
--- frontend/src/app/modelling/rdf-models/generic-odp-model.service.ts
+++ frontend/src/app/modelling/rdf-models/generic-odp-model.service.ts
@@ -17,14 +17,30 @@ export class GenericOdpModelService {
         private dlService: DownloadService,
     ) { }
     
-    getAllClasses(): Observable<Array<string>> {
+    // request data
+    public getAllClasses(): Observable<Array<string>> {
         return this.http.get<SparqlResponse>("lion_BE/generic-odp/all-classes").pipe(toSparqlVariableList(), take(1));
     }
-
-    getAllIndividualsOfClass(classIri: string): Observable<Array<string>> {
-        const params = new HttpParams()
-            .append("classIri", classIri)
-        return this.http.get<SparqlResponse>("lion_BE/generic-odp/all-ind-of-class", {params: params}).pipe(toSparqlVariableList(), take(1));
+    public getAllIndividuals(): Observable<Array<string>> {
+        return this.http.get<SparqlResponse>("lion_BE/generic-odp/all-ind").pipe(toSparqlVariableList(), take(1));
     }
+    public getAllIndividualsOfClass(classIri: string): Observable<Array<string>> {
+        const params = new HttpParams()
+            .append("classIri", classIri);
+        return this.http.get<SparqlResponse>("lion_BE/generic-odp/ind-of-class", {params: params}).pipe(toSparqlVariableList(), take(1));
+    }
+    public getAllObjectPropertiesForDomainIndividual(individualIri: string): Observable<Array<string>> {
+        const params = new HttpParams()
+            .append("individualIri", individualIri);
+        return this.http.get<SparqlResponse>("lion_BE/generic-odp/op-for-dom-ind", {params: params}).pipe(toSparqlVariableList(), take(1));
+    }
+    public getAllRangeIndividualsForObjectProperty(objectPropertyIri: string): Observable<Array<string>> {
+        const params = new HttpParams()
+            .append("objectPropertyIri", objectPropertyIri);
+        return this.http.get<SparqlResponse>("lion_BE/generic-odp/rng-ind-for-op", {params: params}).pipe(toSparqlVariableList(), take(1));
+    }
+
+    // create triples
+    //
 
 }
