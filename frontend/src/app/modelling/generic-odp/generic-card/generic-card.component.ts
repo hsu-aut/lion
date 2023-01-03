a08e31840b61efcf0db3878a112bd7b44fc66328
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-01-03 13:45:39 +0100

    frontend updates

--- frontend/src/app/modelling/generic-odp/generic-card/generic-card.component.ts
+++ frontend/src/app/modelling/generic-odp/generic-card/generic-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output, Type, ViewChild } from '@angular/core';
 import { GenericCardContentComponent } from './generic-card-content.component';
-import { GenericCardConfig } from './generic-card-implementations/generic-card-config.interface';
+import { GenericCardConfig } from '../generic-odp-config.interface';
 import { GenericCardImplementationsService } from './generic-card-implementations/generic-card-implementations.service';
 import { GenericCardContentDirective } from './generic-card-content.directive';
 
@@ -13,8 +13,12 @@ export class GenericCardComponent implements OnInit {
   @ViewChild(GenericCardContentDirective, {static: true}) genericCardContent!: GenericCardContentDirective;
 
   @Input() public config: GenericCardConfig;
-  
+  @Input() public editMode: boolean;
+
   @Output("onRemoveCard") onRemoveCard = new EventEmitter<number>();
+  @Output("onMoveCardUp") onMoveCardUp = new EventEmitter<number>();
+  @Output("onMoveCardDown") onMoveCardDown = new EventEmitter<number>();
+
 
   constructor( private genericCardImplementationsService: GenericCardImplementationsService) { }
 
@@ -28,6 +32,23 @@ export class GenericCardComponent implements OnInit {
 
   removeCard(): void {
     this.onRemoveCard.emit(this.config.id);
+  }
+
+  moveCardUp(): void {
+    this.onMoveCardUp.emit(this.config.id);
+  }
+
+  moveCardDown(): void {
+    this.onMoveCardDown.emit(this.config.id);
+  }
+
+  toggleEditMode(): void {
+    this.editMode = !this.editMode;
+  }
+
+  changeDescriptionText(newDescriptionText: string): void {
+    console.log(newDescriptionText)
+    this.config.descriptionText = newDescriptionText;
   }
 
 }
