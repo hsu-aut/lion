2335833074ade0af5ce4873232fdd32cb2974741
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2023-04-06 14:45:01 +0200

    refactor(frontend): :coffin: removed dead code
    Removed old message code that was commented out, removed style that was not used anymore, added short comment to rewritten message component

--- frontend/src/app/core/main-layout/messaging/message/messages.component.ts
+++ frontend/src/app/core/main-layout/messaging/message/messages.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { Message } from '../message-container/message-container.component';
 
+/**
+ * This is a pretty simple component that gets passed a message object and displays it in HTML
+ */
 @Component({
     selector: 'message',
     templateUrl: './messages.component.html',
