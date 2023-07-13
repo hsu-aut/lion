7c89e0aeb558e013c25e700670121bb677981a41
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2023-07-13 16:50:10 +0200

    fix(frontend): :bug: minor fix in table

--- frontend/src/app/shared/modules/table/table.module.ts
+++ frontend/src/app/shared/modules/table/table.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { TableComponent } from './table.component';
 
 @NgModule({
