0cc02cb25761f83751de9e7a1ce860a24aa0fa2b
aljoshaKoecher <50097079+aljoshakoecher@users.noreply.github.com>
2023-07-13 16:49:26 +0200

    refactor(backend): :recycle: moved vdi2206 to backend

--- backend/src/routes/vdi2206/vdi2206.service.ts
+++ backend/src/routes/vdi2206/vdi2206.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { SparqlService } from '../../shared-services/sparql.service';
 import { GraphOperationService } from '../../shared-services/graph-operation.service';
 import { SparqlResponse } from '../../models/sparql/SparqlResponse';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable()
 export class Vdi2206Service {
@@ -14,14 +14,20 @@ export class Vdi2206Service {
 
 
 	public selectAllSystems(): Observable<SparqlResponse> {
-		const queryString	= `
-			PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+		const queryString	= 
+			`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-			
-			SELECT ?System WHERE {
-				?System a VDI2206:System.
+
+			SELECT ?system ?systemLabel ?consistsOfEntity ?EntityType WHERE {
+				?system a VDI2206:System.
+				OPTIONAL {
+					?system rdfs:label ?systemLabel.
+					?System VDI2206:consistsOf ?consistsOfEntity.
+					?consistsOfEntity rdf:type ?EntityType.
+					VALUES ?EntityType {VDI2206:System VDI2206:Module VDI2206:Component}
+				}
 			}`;
-		return this.queryService.query(queryString);
+		return this.queryService.query(queryString).pipe(tap(data => console.log(data.results.bindings)));
 	} 
 
 
@@ -30,8 +36,13 @@ export class Vdi2206Service {
 			`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 			
-			SELECT ?Module WHERE {
-				?Module a VDI2206:Module.
+			SELECT ?module ?consistsOfEntity ?EntityType WHERE {
+				?module a VDI2206:Module.
+				OPTIONAL {
+					?module VDI2206:consistsOf ?consistsOfEntity.
+					?consistsOfEntity rdf:type ?EntityType.
+					VALUES ?EntityType {VDI2206:Module VDI2206:Component}
+				}
 			}`;
 		
 		return this.queryService.query(queryString);
@@ -59,7 +70,7 @@ export class Vdi2206Service {
 		const queryString = 
 			`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
 			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-			
+					
 			SELECT ?Component WHERE {
 				?Component a VDI2206:Component.
 			}`;
@@ -68,81 +79,92 @@ export class Vdi2206Service {
 	}
 
 
-	/**
-	 * Selects all systems and the sub-systems, modules, and components that they consist of
-	 * @returns 
-	 */
-	public selectSystemsAndConstituents(): Observable<SparqlResponse>  {
+	// /**
+	//  * Selects all systems and the sub-systems, modules, and components that they consist of
+	//  * @returns 
+	//  */
+	// public selectSystemsAndConstituents(): Observable<SparqlResponse>  {
+	// 	const queryString = 
+	// 		`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+	// 		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+	// 		SELECT ?System ?systemLabel ?consistsOfEntity ?EntityType WHERE {
+	// 			?System a VDI2206:System.
+	// 			OPTIONAL {
+	// 				?system rdfs:label ?systemLabel.
+	// 				?System VDI2206:consistsOf ?consistsOfEntity.
+	// 				?consistsOfEntity rdf:type ?EntityType.
+	// 				VALUES ?EntityType {VDI2206:System VDI2206:Module VDI2206:Component}
+	// 			}
+	// 		}`;
+
+	// 	return this.queryService.query(queryString);
+	// } 
+	
+	
+	// public selectModulesAndConstituents(): Observable<SparqlResponse> {
+	// 	const queryString = `
+	// 		PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+	// 		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			
+	// 		SELECT ?Module ?consistsOfEntity ?EntityType WHERE {
+	// 			?Module a VDI2206:Module.
+	// 			OPTIONAL {
+	// 				?Module VDI2206:consistsOf ?consistsOfEntity.
+	// 				?consistsOfEntity rdf:type ?EntityType.
+	// 				VALUES ?EntityType {VDI2206:Module VDI2206:Component}
+	// 			}
+	// 		}`;
+
+	// 	return this.queryService.query(queryString);
+	// } 
+	
+
+	// public selectSystemInheritanceInformation() {
+	// 	const queryString = `
+	// 		PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+	// 		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			
+	// 		SELECT ?System ?childEntity ?childEntityType WHERE {
+	// 			?System a VDI2206:System.
+	// 			OPTIONAL {
+	// 				?System VDI2206:hasChild ?childEntity.
+	// 				?childEntity rdf:type ?childEntityType.
+	// 				VALUES ?childEntityType {VDI2206:System VDI2206:Module VDI2206:Component}
+	// 			}
+	// 		}`;
+	// 	return this.queryService.query(queryString);
+	// } 
+
+	public selectModuleInheritanceInformation() {
 		const queryString = 
-		`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
-		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
-		SELECT ?System ?systemLabel ?consistsOfEntity ?EntityType WHERE {
-			?System a VDI2206:System.
-			OPTIONAL {
-				?system rdfs:label ?systemLabel.
-				?System VDI2206:consistsOf ?consistsOfEntity.
-				?consistsOfEntity rdf:type ?EntityType.
-				VALUES ?EntityType {VDI2206:System VDI2206:Module VDI2206:Component}
-			}
-		}`;
-
-		return this.queryService.query(queryString);
-	} 
-	
-	
-	public selectModulesAndConstituents(): Observable<SparqlResponse> {
-		const queryString = `
-		PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
-		PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 		
-		SELECT ?Module ?consistsOfEntity ?EntityType WHERE {
-			?Module a VDI2206:Module.
-			OPTIONAL {
-				?Module VDI2206:consistsOf ?consistsOfEntity.
-				?consistsOfEntity rdf:type ?EntityType.
-				VALUES ?EntityType {VDI2206:Module VDI2206:Component}
-			}
-		}`;
-
+			SELECT ?Module ?childEntity ?childEntityType WHERE {
+				?Module a VDI2206:Module.
+				OPTIONAL {
+					?Module VDI2206:hasChild ?childEntity.
+					?childEntity rdf:type ?childEntityType.
+					VALUES ?childEntityType {VDI2206:Module VDI2206:Component}
+				}
+			}`;
 		return this.queryService.query(queryString);
 	} 
-	
-	// TODO: Check if these functions are really needed...
 
-	// 	public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_SYS = `
-	// PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
-	// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-	
-	// SELECT ?System ?childEntity ?childEntityType WHERE {
-	// ?System a VDI2206:System.
-	//   OPTIONAL {?System VDI2206:hasChild ?childEntity.
-	// 	?childEntity rdf:type ?childEntityType.
-	// 	VALUES ?childEntityType {VDI2206:System VDI2206:Module VDI2206:Component}
-	// }
-	// }
-	// `
-	// 	public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_MOD = `
-	// PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
-	// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-	
-	// SELECT ?Module ?childEntity ?childEntityType WHERE {
-	// ?Module a VDI2206:Module.
-	//   OPTIONAL {?Module VDI2206:hasChild ?childEntity.
-	// 	?childEntity rdf:type ?childEntityType.
-	// 	VALUES ?childEntityType {VDI2206:Module VDI2206:Component}}
-	// }
-	// `
-	// 	public SPARQL_SELECT_STRUCTUAL_INFO_BY_INHERITANCE_BY_COM = `
-	// PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
-	// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-	
-	// SELECT ?Component ?childEntity ?childEntityType WHERE {
-	// ?Component a VDI2206:Component.
-	//   OPTIONAL {?Component VDI2206:hasChild ?childEntity.
-	// 	?childEntity rdf:type ?childEntityType.
-	// 	VALUES ?childEntityType {VDI2206:Component}}
-	// }
-	`
+	public selectComponentInheritanceInformation() {
+		const queryString = 
+			`PREFIX VDI2206: <http://www.hsu-ifa.de/ontologies/VDI2206#>
+			PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+			
+			SELECT ?Component ?childEntity ?childEntityType WHERE {
+			?Component a VDI2206:Component.
+				OPTIONAL {
+					?Component VDI2206:hasChild ?childEntity.
+					?childEntity rdf:type ?childEntityType.
+					VALUES ?childEntityType {VDI2206:Component}
+				}
+			}`;
+		return this.queryService.query(queryString);}
 	
 }
