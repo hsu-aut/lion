fcbcedc4eb5659887bfd20c3a3fe86497ae8c7ed
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-10-05 14:41:23 +0200

    added redirect to lions story

--- frontend/src/app/core/main-layout/main-layout-routing.module.ts
+++ frontend/src/app/core/main-layout/main-layout-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './main-layout.component';
-
-
 // import { ModellingGuideComponent } from './modellingGuide/modellingGuide.component';
-
 
 const routes: Routes = [
     {
         path: '',
         component: MainLayoutComponent,
         children: [
+            { path: '', redirectTo: 'lionStory', pathMatch: 'prefix' },
             { path: 'lionStory', loadChildren: () => import('../../features/lionStory/lionStory.module').then(m => m.LionStoryModule)},
             { path: 'configuration',  loadChildren: () => import('../../features/configuration/configuration.module').then(m => m.ConfigurationModule)},
             { path: 'modelling', loadChildren: () => import('../../features/modelling/modelling.module').then(m => m.ModellingModule)},
