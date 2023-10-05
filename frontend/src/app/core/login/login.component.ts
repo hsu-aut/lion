d84fc5f1fa6bc78ce6ce13e4c7cb7f46af9e9f5b
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-10-05 14:41:23 +0200

    added login logic

--- frontend/src/app/core/login/login.component.ts
+++ frontend/src/app/core/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../../router.animations';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { SignInDtoReq } from '@shared/models/SignInDto';
+import { AuthService } from '../../shared/auth/auth.service';
 
 @Component({
     selector: 'app-login',
@@ -9,13 +12,50 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
+
+    public signInForm: FormGroup;
+
     constructor(
-      public router: Router
-    ) {}
+      public router: Router,
+      private fb: FormBuilder,
+      private authService: AuthService,
+    ) {
+        this.signInForm = this.fb.group({
+            inputEmail: ['',Validators.required],
+            inputPassword: ['',Validators.required]
+        });
+    }
 
     ngOnInit() {}
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
+    /**
+     * gets data from sign in form and calls AuthService to sign in user.
+     */
+    onLogin(): void {
+
+        const formValues = this.signInForm.value;
+        if (!formValues.inputEmail || !formValues.inputPassword) {
+            // TODO call message service instead of console
+            console.error("Login error, provide username and password");
+            return;
+        }
+
+        const signinData: SignInDtoReq = {
+            username: formValues.inputEmail,
+            password: formValues.inputPassword
+        };
+
+        this.authService.signIn(signinData).subscribe( (signedIn: boolean) => {
+            if (!signedIn) {
+                this.signInForm.reset();
+                // TODO call message service instead of console
+                console.error("Login error, username and/or password incorrect");
+            }
+            this.router.navigate(['main']);
+        });
+        
+        return;
+
     }
+
 }
