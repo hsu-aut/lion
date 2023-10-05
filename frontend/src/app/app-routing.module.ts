96b339ca254325289dc3feee0e5c86b4e808680d
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-10-05 14:41:23 +0200

    updated imports and routes, enabled route guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from './shared';
-
+import { AuthGuard } from './shared/auth/auth.guard';
+import { AuthModule } from './shared/auth/auth.module';
 
 const routes: Routes = [
     // { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
     { path: '', loadChildren: () => import('./core/login/login.module').then(m => m.LoginModule)},
-    { path: 'main', loadChildren: () => import('./core/main-layout/main-layout.module').then(m => m.MainLayoutModule)},
+    { path: 'main', loadChildren: () => import('./core/main-layout/main-layout.module').then(m => m.MainLayoutModule), canActivateChild: [AuthGuard]},
     { path: 'not-found', loadChildren: () => import('./core/not-found/not-found.module').then(m => m.NotFoundModule)},
     { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [
+        RouterModule.forRoot(routes),
+        AuthModule
+    ],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
