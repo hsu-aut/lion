59bc9840c6ad7f25a9cb0f77bee6be733a8d3e44
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-11-30 15:14:25 +0100

    updated dtos for auth and repositories

--- shared/models/AuthDtos.ts
+++ shared/models/AuthDtos.ts
@@ -2,6 +2,13 @@ export interface SignInDtoReq {
     username: string,
     password: string,
 }
+
 export interface SignInDtoRes {
     access_token: string
+}
+
+export interface SignUpDto {
+    username: string,
+    password: string,
+    email: string
 }
