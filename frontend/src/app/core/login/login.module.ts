59c6aa37712d029c9be2e5061638b325870a1845
Max Weigand <84445377+mxweigand@users.noreply.github.com>
2023-11-30 15:14:23 +0100

    updated imports and providers

--- frontend/src/app/core/login/login.module.ts
+++ frontend/src/app/core/login/login.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../shared/auth/auth.service';
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
         LoginRoutingModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        AuthService,
     ],
     declarations: [LoginComponent]
 })
